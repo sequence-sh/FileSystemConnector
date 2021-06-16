870904a7b9139990ee46380a770e91c7bec3a60f
Mark Wainwright <wainwrightml@gmail.com>
2021-06-16 10:03:21 +0100

    Added more tests for DirectoryGetItems

--- FileSystem.Tests/DirectoryGetItemsTests.cs
+++ FileSystem.Tests/DirectoryGetItemsTests.cs
@@ -20,7 +20,56 @@ public partial class DirectoryGetItemsTests : StepTestBase<DirectoryGetItems, Ar
                     Entity.Create(("Name", "Alpha.txt"), ("Type", "File")),
                     Entity.Create(("Name", "temp"),      ("Type", "Directory"))
                 }.ToSCLArray()
-            ).WithFileSystem(initialFiles: new[] { ("Alpha.txt", "a") });
+            ).WithFileSystem(new[] { ("Alpha.txt", "a") });
+
+            yield return new StepCase(
+                "Nested one level",
+                new DirectoryGetItems() { Directory = StaticHelpers.Constant("myDir") },
+                new[] { Entity.Create(("Name", "Alpha.txt"), ("Type", "File")) }.ToSCLArray()
+            ).WithFileSystem(new[] { ("myDir/Alpha.txt", "a") });
+
+            yield return new StepCase(
+                "Simple pattern",
+                new DirectoryGetItems()
+                {
+                    Directory = StaticHelpers.Constant("myDir"),
+                    Pattern   = StaticHelpers.Constant("*.txt")
+                },
+                new[] { Entity.Create(("Name", "Alpha.txt"), ("Type", "File")) }.ToSCLArray()
+            ).WithFileSystem(new[] { ("myDir/Alpha.txt", "a"), ("myDir/Alpha.jpg", "a"), });
+
+            yield return new StepCase(
+                "Recursive Case",
+                new DirectoryGetItems() { Directory = StaticHelpers.Constant("dira") },
+                new[]
+                {
+                    Entity.Create(("Name", "Alpha.txt"), ("Type", "File")),
+                    Entity.Create(
+                        ("Name", "dirb"),
+                        ("Type", "Directory"),
+                        ("Children",
+                         new List<Entity>()
+                         {
+                             Entity.Create(("Name", "Beta.txt"), ("Type", "File")),
+                         })
+                    ),
+                    Entity.Create(
+                        ("Name", "dirc"),
+                        ("Type", "Directory"),
+                        ("Children",
+                         new List<Entity>()
+                         {
+                             Entity.Create(("Name", "Gamma.txt"), ("Type", "File")),
+                         })
+                    ),
+                }.ToSCLArray()
+            ).WithFileSystem(
+                new[]
+                {
+                    ("dira/Alpha.txt", "a"), ("dira/dirb/Beta.txt", "a"),
+                    ("dira/dirc/Gamma.txt", "a"),
+                }
+            );
         }
     }
 }
